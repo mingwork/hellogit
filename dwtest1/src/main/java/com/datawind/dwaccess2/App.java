46937ec5b3b00a39926659f0b6d0303fddaae6c8
LU <LU@10.0.0.135>
2016-05-03 17:05:58 -0400

    first commit

---
+++ dwtest1/src/main/java/com/datawind/dwaccess2/App.java
@@ -0,0 +1,14 @@
+package com.datawind.dwaccess2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World2!" );
+    }
+}
